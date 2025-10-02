458e14c77a82ef881771c3128900b1d048640df6
Pavel Neverov <140485683+3GonoverskyiPoSkidkeAndMalboroRed@users.noreply.github.com>
2025-10-02 09:47:49 +0300

    убран

--- frontend/src/components/method/MethodFooter.vue
+++ frontend/src/components/method/MethodFooter.vue
@@ -1,24 +1,6 @@
 <template>
   <div class="bg-white border-t border-gray-200">
-    <div class="max-w-4xl mx-auto px-4 py-6">
-      <p class="text-gray-600 text-sm">
-        Более подробную информацию можно получить на сайте 
-        <a 
-          href="https://www.gost.ru" 
-          target="_blank" 
-          class="text-blue-600 hover:text-blue-800 underline"
-        >
-          Федерального агентства по техническому регулированию и метрологии
-        </a> 
-        и на страницах 
-        <a 
-          href="/" 
-          class="text-blue-600 hover:text-blue-800 underline"
-        >
-          нашего сайта
-        </a>.
-      </p>
-    </div>
+    
   </div>
 </template>
 
